bae08d5cd34c32356ec56e296c58e0b9c95979d3
Adam Stebbins <acstebbins7@gmail.com>
2021-03-03 09:12:08 -0500

    Git PUSH

--- color-combinator.js
+++ color-combinator.js
@@ -1,7 +1,22 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function colorCombinator(str1, str2) {
+  if(
+  (str1 === "red" && str2 === "yellow") || (str1 === "yellow" && str2 === "red")
+  ){
+  return "orange"
+}
 
+if(
+ (str1 === "red" && str2 === "blue") || (str1 === "blue" && str2 === "red")
+ )
+ return "purple"
+if(
+ (str1 === "yellow" && str2 === "blue") || (str1 === "blue" && str2 === "yellow")
+ )
+ return "green"
+}
 
 
 
--- color-deconstructor.js
+++ color-deconstructor.js
@@ -1,7 +1,17 @@
 /******************
  * YOUR CODE HERE *
  ******************/
-
+function colorDeconstructor(str) {
+  if(str === "orange") {
+    return "red and yellow"
+  } 
+  if(str === "purple") {
+    return "red and blue"
+  }
+  if(str === "green") {
+    return "blue and yellow"
+  }
+}
 
 
 
